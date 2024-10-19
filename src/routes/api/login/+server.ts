a13e31ae3e2e5503e2dc25d63d74df8aa5635794
averieyy <58344917+averieyy@users.noreply.github.com>
2024-10-19 12:05:41 +0200

    added basic login form and started 2fa

---
+++ src/routes/api/login/+server.ts
@@ -0,0 +1,33 @@
+import type { RequestHandler } from "@sveltejs/kit";
+import { createTransport } from 'nodemailer';
+import 'dotenv/config';
+
+const transport = createTransport({
+  host: process.env.EMAILHOST,
+  port: parseInt(process.env.EMAILPORT || '587') || 587,
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.EMAILPASS,
+  }
+});
+
+export const POST: RequestHandler = async ({ cookies, request }) => {
+  const respond = (data:object, status: number = 200) => new Response(JSON.stringify(data), { status });
+
+  const { email, password, username } : { email: string, password: string, username: string } = await request.json();
+
+  if (!email || !password) return respond({message: 'Email or password missing'}, 400);
+
+  const token = Math.floor(Math.random() * 1000000);
+
+  transport.sendMail({
+    from: process.env.EMAIL,
+    to: email,
+    subject: `2FA - ${username}`,
+    text: `Your 2FA token is ${token.toString().padStart(6,'0')}`
+  });
+
+  return new Response('', {status: 302, headers: {
+    'Location': '/2fa'
+  }});
+}
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  let email: string;
+  let password: string;
+  let username: string;
+
+  function submit () {
+    fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        username
+      })
+    }).then(resp => {
+      
+    });
+  }
+</script>
+
+<main>
+  <form on:submit={submit}>
+    <h2>Log in</h2>
+    <input bind:value={username} type="text" placeholder="Username">
+    <input bind:value={email} type="email" placeholder="Email">
+    <input bind:value={password} type="password" placeholder="Password">
+    <input type="submit" value="Log in">
+  </form>
+</main>
+
+<style>
+  main {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  form {
+    display: flex;
+    flex-direction: column;
+    background-color: var(--bg2);
+    gap: .5rem;
+    padding: .5rem;
+  }
+
+  input {
+    background-color: var(--bg3);
+    color: var(--fg1);
+    border: none;
+    padding: .5rem;
+  }
+</style>
